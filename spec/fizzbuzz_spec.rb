4c09a6b894f78807e650a2d832398b25fc513638
Ruth Baker <ruthmoog@gmail.com>
2019-07-19 16:27:49 +0100

    refactor: Combine tests for multiples of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,8 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
-  end
-  it 'returns "fizz" when passed 6' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
   it 'returns "buzz" when passed 5' do
