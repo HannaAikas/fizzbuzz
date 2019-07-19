e26873f5acb068f9cbb9b4dfb56a7f98517cbf2a
Hanna <hannaaikas@gmail.com>
2019-07-19 19:53:01 +0100

    feat: return number when given number that is not a multiple of 3 or 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,7 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+  it 'returns the number for numbers that are not multiples of 3 or 5' do
+    expect(fizzbuzz(11)).to eq 11
+  end
 end
