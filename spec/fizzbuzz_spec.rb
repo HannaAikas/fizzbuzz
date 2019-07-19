596ba35bc867201d5e069ee156b87d84c2b3abe5
Hanna <hannaaikas@gmail.com>
2019-07-19 19:47:02 +0100

    test: return 'buzz' when given multiple of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,7 +5,8 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
     expect(fizzbuzz(6)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed 5' do
+  it 'returns "buzz" when passed a multiple of 10' do
     expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
   end
 end
